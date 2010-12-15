5dc196ae652e6c756550b1646144d28d28139729
Li Xuanji <xuanji@gmail.com>
2010-12-15 17:06:36 +0800

    extracted expand.c

---
+++ nush-testcase/expand.c
@@ -0,0 +1,41 @@
+#include "klee/klee.h"
+
+int main() {
+  
+  char arg_buf[4];
+  unsigned char buffer_buf[100];
+
+  klee_make_symbolic(arg_buf, sizeof(arg_buf), "arg");
+
+  char *arg = arg_buf;
+  unsigned char *buffer = buffer_buf;
+
+  int i, ac;
+
+  while (*arg) {
+        if (*arg == '\\') {
+                arg++;
+                i = ac = 0;
+                if (*arg >= '0' && *arg <= '7') {
+                        do {
+                                ac = (ac << 3) + *arg++ - '0';
+                                i++;
+                        } while (i < 4 && *arg >= '0' && *arg <= '7');
+                        *buffer++ = ac;
+                } else if (*arg != '\0')
+                        *buffer++ = *arg++;
+        } else if (*arg == '[') {
+                arg++;
+                i = *arg++;
+                if (*arg++ != '-') {
+                        *buffer++ = '[';
+                        arg -= 2;
+                        continue;
+                }
+                ac = *arg++;
+                while (i <= ac) *buffer++ = i++;
+                arg++;          /* Skip ']' */
+        } else
+                *buffer++ = *arg++;
+  }
+}
