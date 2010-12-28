2aba6fb19d5572c899b12b20236f70adb3a55fce
Li Xuanji <xuanji@gmail.com>
2010-12-28 16:35:43 +0800

    +data, +picture

---
+++ nush-testcase/sample_prog.c
@@ -0,0 +1,17 @@
+int main() {
+  int a,b,c;
+  if (a) {
+    if (b) {
+      c++;
+    }
+    b++;
+  }
+  else {
+    while (b --> 0) {
+      if (c)
+        a++;
+    }
+    a++;
+  }
+  c+=a;
+}
